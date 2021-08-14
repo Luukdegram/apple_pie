3aef19a62240e880e02040bae2792625fbc09664
Meghan Denny <hello@nektro.net>
2021-08-14 01:29:08 -0700

    fix extra case caught by tests

--- src/Request.zig
+++ src/Request.zig
@@ -287,7 +287,7 @@ const FormIterator = struct {
                 var batch_name: [72]u8 = undefined;
                 std.mem.copy(u8, &batch_name, "--");
                 std.mem.copy(u8, batch_name[2..], boundary_name);
-                var batch_it = std.mem.split(data, batch_name[0 .. boundary_name.len + 2]);
+                var batch_it = std.mem.split(u8, data, batch_name[0 .. boundary_name.len + 2]);
 
                 var batch = batch_it.next() orelse return error.InvalidBody;
                 batch = batch_it.next() orelse return error.InvalidBody; // Actually get the batch as first one has len 0
